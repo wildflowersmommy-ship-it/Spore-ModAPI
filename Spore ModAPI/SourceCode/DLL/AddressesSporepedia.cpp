fcf44437bd826c610537e8ed844eb61b3729944a
wildflowersmommy-ship-it <wildflowersmommy@gmail.com>
2026-05-08 19:50:41 -0500

    Fix namespace declaration for Sporepedia addresses

--- Spore ModAPI/SourceCode/DLL/AddressesSporepedia.cpp
+++ Spore ModAPI/SourceCode/DLL/AddressesSporepedia.cpp
@@ -8,7 +8,7 @@
 #include <Spore\Sporepedia\cSPUILargeAssetView.h>
 #include <Spore\Sporepedia\ISPUILargeAssetView.h>
 
-namespace Sporepedia
+namespace Biocards
 {
 	namespace Addresses(ShopperRequest) 
 	{
